43b0fff1234ec295ebe0babfed24fd18f5088c6a
adam-coates <>
2026-04-07 21:56:28 +0200

    add quarto flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Quarto development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        pythonPackages = with pkgs.python313Packages; [
+          jupyter
+          numpy
+          pandas
+          matplotlib
+        ];
+        rPackages = with pkgs.rPackages; [
+          ggplot2
+          dplyr
+          knitr
+          rmarkdown
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            quarto
+            pandoc
+            texliveMedium
+            python313
+            R
+          ] ++ pythonPackages ++ rPackages;
+        };
+      }
+    );
+}

552f30232e59ff997aae7e1a34432b298201c8bc
adam-coates <123807847+adam-coates@users.noreply.github.com>
2026-05-10 16:29:21 +0200

    add pandiff as its useful

--- flake.nix
+++ flake.nix
@@ -12,6 +12,26 @@
       system:
       let
         pkgs = inputs.nixpkgs.legacyPackages.${system};
+        pandiff = pkgs.buildNpmPackage {
+          pname = "pandiff";
+          version = "0.8.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "davidar";
+            repo = "pandiff";
+            rev = "2631bb939e8031c4feb04a4b76e7df600e442a40";
+            hash = "sha256-GLLzfduUlTjeVc6AfifTXGJO/7elHbHv5XZeBdKjN5I=";
+          };
+          postPatch = ''
+            cp ${./pandiff-package-lock.json} package-lock.json
+            substituteInPlace package.json --replace-fail '"prepare": "yarn run compile",' ""
+          '';
+          npmDepsHash = "sha256-VNnrSzmb+gcJO+V6QCQg/IVlsGPrjEg4gIFDEb+7jDM=";
+          npmBuildScript = "compile";
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postInstall = ''
+            wrapProgram $out/bin/pandiff --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.pandoc ]}
+          '';
+        };
         pythonPackages = with pkgs.python313Packages; [
           jupyter
           numpy
@@ -33,6 +53,7 @@
             [
               quarto
               pandoc
+              pandiff
               texliveMedium
               python313
               R
